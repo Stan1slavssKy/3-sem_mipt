60c4ed512097a589f7754cfa806a82be15e9f595
stanislav <stas00333@gmail.com>
2021-10-02 10:18:40 +0300

    fifo

---
+++ semenars/sem_02.10.2021/cl2.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
+void client_2 ();
+
+int main ()
+{
+    (void) umask (0);
+
+    if(mknod("fir_fifo", S_IFIFO | S_IRUSR | S_IWUSR, 0) == - 1)
+    {
+        if (errno != EEXIST)
+        {
+            fprintf (stderr, "Cant create fifo %d\n", __LINE__);
+        }
+    }
+
+    if(mknod("sec_fifo", S_IFIFO | S_IRUSR | S_IWUSR, 0) == - 1)
+    {
+        if (errno != EEXIST)
+        {
+            fprintf (stderr, "Cant create fifo %d\n", __LINE__);
+        }
+    }
+
+    client_2 ();
+}
+
+void client_2 ()
+{
+    pid_t pid = fork ();
+    if (pid == 0)
+    {
+        int fd = open ("sec_fifo", O_RDONLY);
+        
+        char buffer[1000] = {};
+        fgets (buffer, 1000, stdin);
+        read (fd, buffer, 1000);
+        
+        printf ("%s\n", buffer);
+
+        close (fd);
+    }
+    else if (pid > 0)
+    {
+        int fd = open ("fir_fifo", O_RDONLY);
+        
+        char buffer[1000] = {};
+        fgets (buffer, 1000, stdin);
+        write (fd, buffer, 1000);
+        
+        close (fd);
+    }
+    else if (pid == -1) 
+    {
+        fprintf (stderr, "fork error %d", __LINE__);
+        exit (0);
+    }
+}
